b2500d6a0ada475d6508e30f553d9215ba11e8a0
Sp3ciaL2X <Sp3ciaL2X@gmail.com>
2019-11-26 08:16:48 +0300

    Update example.php

--- example.php
+++ example.php
@@ -5,8 +5,8 @@
  * @author 		Sp3ciaL2X <Sp3ciaL2X@gmail.com>
  * @since 		2019
  * @license		We live in a free world
- * @copyright	By Sp3ciaL2X
- * @version 	1.0.0
+ * @copyright		By Sp3ciaL2X
+ * @version		1.0.0
  *
  **/
 
